1d811dbb205757bcb0974776458a974eb03a8b18
shivam <shivamt0407@gmail.com>
2025-01-13 22:50:15 +0530

    refactor:checking whether the user is a teacher or not

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,3 +1,4 @@
+import { isTeacher } from "@/actions/isTeacher";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -5,9 +6,9 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const { userId } = await auth();
-    const {title} = await req.json();
+    const { title } = await req.json();
 
-    if (!userId) {
+    if (!userId || isTeacher(userId)) {
       return new NextResponse("Unauthorized aceess", { status: 401 });
     }
 
